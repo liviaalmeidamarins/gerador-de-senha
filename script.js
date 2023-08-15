e4f1067ec050e382e2c926f8051696be19075c27
liviaalmeidamarins <lialmeidamarins@gmail.com>
2023-08-15 13:49:14 -0300

    frat: gerador de senha

---
+++ script.js
@@ -0,0 +1,10 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElemt = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#avalor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector('#container-password');
+
+let charset = ""
+let novaSenha = ""
